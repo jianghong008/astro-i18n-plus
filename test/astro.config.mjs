b1e8d12715c6f6cff5d842ad1a4dc13658e52775
jianghong <jianghong2020@qq.com>
2023-10-10 18:30:33 +0800

    tailwind测试

--- test/astro.config.mjs
+++ test/astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 import astroI18nPlus from 'astro-i18n-plus';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    integrations:[astroI18nPlus('zh'),]
+  integrations: [astroI18nPlus('zh'), tailwind()]
 });
